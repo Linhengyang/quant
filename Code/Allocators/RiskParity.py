e854b407de2c94ed699adfe86ae7434ae544c80d
lhy-Macbook01 <linhengyang1@outlook.com>
2023-12-15 18:50:47 +0800

    add

--- Code/Allocators/RiskParity.py
+++ Code/Allocators/RiskParity.py
@@ -10,7 +10,29 @@
 -------------------------------------------------
 
 """
+import scipy
+import numpy as np
 
 class RiskParity:
     def __init__(self, ):
         pass
+
+    def cal_portf_var(w:np.array, V:np.array):
+        pass
+
+    def cal_risk_contrib_obj(risk_contribs:np.array, tgt_contrib_ratio:np.array=None):
+        # risk_contribs: 各资产/风险因子/资产类别 的trc向量, 即各资产/风险因子/资产类别贡献的风险。
+        # tgt_contrib_ratio: 希望各资产/风险因子/资产类别 风险贡献的目标比例。不输入时，默认各资产/风险因子/资产类别平均分担总风险
+        num_risk = len(risk_contribs)
+        if tgt_contrib_ratio is None:
+            tgt_contrib_ratio = np.array([ 1.0/num_risk ]*num_risk)
+        assert len(tgt_contrib_ratio) == num_risk, "risk_contribs and tgt_contrib_ratio must have same length"
+        assert np.abs( tgt_contrib_ratio.sum() - 1 ) <= 0.01, "tgt_contrib_ratio must be summed to 1"
+
+        
+
+
+if __name__ == "__main__":
+    x = np.array([1/7] * 7)
+    y = x.sum()
+    print( y )
