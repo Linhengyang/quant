9b8a8a0fe67b5cf7bde6c18069bc839e11ff16b4
lhy-Macbook01 <linhengyang1@outlook.com>
2023-12-15 14:11:48 +0800

    risk parity init

---
+++ Code/Allocators/RiskParity.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+
+-------------------------------------------------
+   File Name:         RiskParity
+   Description :      风险平价：给定各资产波动率，求解波动率平价的portfolio
+   Author :           linhengyang
+   Create date:       2023/12/15
+   Latest version:    v1.0.0
+-------------------------------------------------
+
+"""
+
+class RiskParity:
+    def __init__(self, ):
+        pass
