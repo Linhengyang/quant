dca76b08d3d38b139c53f03e6b803923ebdd0a7c
lhy-Macbook01 <linhengyang1@outlook.com>
2023-12-29 13:50:21 +0800

    flask app add

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+from werkzeug.middleware.proxy_fix import ProxyFix
+import json
+import re
+import warnings
+from datetime import datetime
+from pprint import pprint
+import traceback
+import sys
+sys.dont_write_bytecode = True
+
+from Code.Forecasters import BlackLitterman
+from Code.Allocators import MeanVarOptimal
+from Code.Allocators import RiskParity
+from Code.Estimators import Risks
+
+warnings.filterwarnings('ignore')
+app = Flask(__name__)
+
+# @app.route('/')
+# def hello():
+#     return 'Welcome to My test flask!'
+
+print(__name__)
--- test.py
+++ test.py
@@ -1,4 +1,1 @@
 import numpy as np
-diag = np.diag([1,2,3,4,5])
-left = np.random.uniform(size=(3,5))
-print(left @ diag @ left.T)
