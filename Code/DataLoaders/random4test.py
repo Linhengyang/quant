6faff9c840ff7eeb25375125a6eb598cfe653fea
lhy-Macbook01 <linhengyang1@outlook.com>
2024-01-02 20:05:19 +0800

    set random seed

--- Code/DataLoaders/random4test.py
+++ Code/DataLoaders/random4test.py
@@ -1,4 +1,4 @@
 import numpy as np
-
+np.random.seed(100)
 def rdm_rtn_data(num_assets:int, back_window_size:int):
     return np.random.uniform(low=-10, high=10, size=(num_assets, back_window_size))
