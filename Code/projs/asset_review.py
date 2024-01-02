641c1518c12c20183ee39f4e2f582fa4a57b4704
lhy-Macbook01 <linhengyang1@outlook.com>
2024-01-02 19:28:33 +0800

    function wrapped

--- Code/projs/asset_review.py
+++ Code/projs/asset_review.py
@@ -1,8 +1,9 @@
 from Code.Forecasters.BlackLitterman import BlackLitterman
 from Code.Allocators.MeanVarOptimal import MeanVarOpt
 from Code.Allocators.RiskParity import RiskParity
-from Code.DataLoaders.random4test import *
+from Code.DataLoaders.random4test import rdm_rtn_data
 
+import numpy as np
 from typing import Callable
 
 
@@ -11,8 +12,10 @@ def load_data_mvopt(low_constraints, high_constraints, rtn_data_loader:Callable,
     rtn_data = rtn_data_loader(**data_kwargs)
     cov_mat = np.cov(rtn_data)
     rtn_rates = rtn_data.mean(axis=1)
-    low_constraints = np.array(low_constraints)
-    high_constraints = np.array(high_constraints)
+    if low_constraints is not None:
+        low_constraints = np.array(low_constraints)
+    if high_constraints is not None:
+        high_constraints = np.array(high_constraints)
     return MeanVarOpt(rtn_rates, cov_mat, low_constraints, high_constraints)
 
 
@@ -26,46 +29,53 @@ def load_data_blkltm(view_pick_mat, view_rtn_vec, bl_args_dict, low_constraints,
     bl_args_dict['hist_cov_mat'] = np.cov(rtn_data)
     bl_model = BlackLitterman(view_pick_mat, view_rtn_vec)
     expct_rtn_rates = bl_model(bl_args_dict)
-    low_constraints = np.array(low_constraints)
-    high_constraints = np.array(high_constraints)
+    if low_constraints is not None:
+        low_constraints = np.array(low_constraints)
+    if high_constraints is not None:
+        high_constraints = np.array(high_constraints)
     return MeanVarOpt(expct_rtn_rates, bl_args_dict['hist_cov_mat'], low_constraints, high_constraints)
 
 
 def mvopt_portf_var_from_r(mvopt:MeanVarOpt, r:np.float32):
-    return mvopt.get_portf_var_from_r(r)
+    var_star, weights_star = mvopt.get_portf_var_from_r(r)
+    res = {'portf_var':var_star, 'portf_w':weights_star}
+    return res
 
 
 def mvopt_portf_r_from_var(mvopt:MeanVarOpt, var:np.float32):
-    return mvopt.get_portf_var_from_r(var)
+    r_star, weights_star = mvopt.get_portf_r_from_var(var)
+    res = {'portf_r':r_star, 'portf_w':weights_star}
+    return res
 
 
 def mvopt_constrained_qp_from_r(mvopt:MeanVarOpt, goal_r:np.float32):
+    # {"portf_w":np_array, "portf_var":float,
+    #  "portf_r":float,    "qp_status":string}
     return mvopt.solve_constrained_qp_from_r(goal_r)
 
 
 def mvopt_constrained_qp_from_var(mvopt:MeanVarOpt, goal_var:np.float32):
+    # {"portf_w":np_array, "portf_var":float,
+    #  "portf_r":float,    "qp_status":string}
     return mvopt.solve_constrained_qp_from_var(goal_var)
 
 # risk parity
 def load_data_riskparity(category_mat, rtn_data_loader:Callable, **data_kwargs):
     rtn_data = rtn_data_loader(**data_kwargs)
+    if category_mat is not None:
+        category_mat = np.array(category_mat)
     return RiskParity(rtn_data, category_mat)
 
 # risk budget
-def load_data_riskbudget(category_mat, tgt_contrib_ratio:np.array, rtn_data_loader:Callable, **data_kwargs):
+def load_data_riskbudget(category_mat, tgt_contrib_ratio, rtn_data_loader:Callable, **data_kwargs):
     rtn_data = rtn_data_loader(**data_kwargs)
+    if category_mat is not None:
+        category_mat = np.array(category_mat)
+    tgt_contrib_ratio = np.array(tgt_contrib_ratio)
     return RiskParity(rtn_data, category_mat, tgt_contrib_ratio)
 
-def risk_asign_portf_w(rp:RiskParity):
-    return rp.optimal_solver()
 
-def risk_asign_actual_risk_contribs(rp:RiskParity):
-    if rp.allocated_weights is None:
-        rp.optimal_solver()
-    return rp.risk_contribs
-
-
-def risk_asign_actual_portf_r(rp:RiskParity):
-    if rp.allocated_weights is None:
-        rp.optimal_solver()
-    return rp.portf_return
+def risk_ctrl(rp:RiskParity):
+    portf_w = rp.optimal_solver()
+    res = {'portf_r':rp.portf_return, 'risk_contribs':rp.risk_contribs, 'portf_w':portf_w}
+    return res
