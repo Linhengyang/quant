0483b5e200b22db336832778ea2b102646b081d2
lhy-Macbook01 <linhengyang1@outlook.com>
2024-01-07 17:41:15 +0800

    Back Test adds

--- asset_allocate_server.py
+++ asset_allocate_server.py
@@ -3,7 +3,7 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 import json
 import re
 import warnings
-from datetime import datetime
+from datetime import datetime, timedelta
 from markupsafe import escape
 from pprint import pprint
 import traceback
@@ -13,6 +13,7 @@ sys.dont_write_bytecode = True
 from Code.projs.asset_allocate.functions import *
 from Code.DataLoader.random4test import rdm_rtn_data
 from Code.DataLoader.remoteDB import db_rtn_data
+from Code.Utils.DateTime import yld_series_dates
 
 warnings.filterwarnings('ignore')
 app_name = __name__
@@ -46,25 +47,38 @@ def application_mvopt_var_from_r():
     return res
 
 
-# @app.route('/BT_mvopt_var_from_r', methods=['POST'])
-# def BT_mvopt_var_from_r():
-#     print('[{}]'.format(datetime.now()))
-#     inputs = request.json
-#     print('Back Test for mean-variance-optimal strategy from {startdate} to {enddate} trading in every {gapday} upon assets {assets}'.format(
-#         startdate=inputs['startdate'], enddate=inputs['enddate'], gapday=inputs['gapday'], assets=inputs['assets_ids']
-#     ))
-#     print('input params: ', inputs)
-#     if 'low_constraints' in inputs:
-#         low_constraints = inputs['low_constraints']
-#     else:
-#         low_constraints = None
-#     if 'high_constraints' in inputs:
-#         high_constraints = inputs['high_constraints']
-#     else:
-#         high_constraints = None
+@app.route('/BT_mvopt_var_from_r', methods=['POST'])
+def BT_mvopt_var_from_r():
+    print('[{}]'.format(datetime.now()))
+    inputs = request.json
+    print('Back Test for mean-variance-optimal strategy from {begindate} to {termidate} trading in every {gapday} upon \
+          assets {assets}'.format(
+        begindate=inputs['begindate'], termidate=inputs['termidate'], gapday=inputs['gapday'], assets=inputs['assets_idx']
+    ))
+    print('input params: ', inputs)
+    if 'low_constraints' in inputs:
+        low_constraints = inputs['low_constraints']
+    else:
+        low_constraints = None
+    if 'high_constraints' in inputs:
+        high_constraints = inputs['high_constraints']
+    else:
+        high_constraints = None
     
-#     portf_w_list, period_rtn_mat_list = [], []
-    
+    portf_w_list, period_rtn_mat_list = [], []
+    res_list = []
+    begindate, termidate, gapday, back_window_size = inputs['begindate'], inputs['termidate'], \
+        int(inputs['gapday']), inputs['back_window_size']
+    for trade_dt in yld_series_dates(begindate, termidate, gapday):
+        # trade_dt是begindate开始，到termidate为止或之前，每gapday的日期。即交易日期。
+        startdate = datetime.strptime(trade_dt, "%Y%m%d") + timedelta(days= -back_window_size)
+        enddate = datetime.strptime(trade_dt, "%Y%m%d") + timedelta(days= -1)
+        res = mvopt_portf_var_from_r(inputs['expt_rtn_rate'], low_constraints, high_constraints, 
+                                     rtn_data_loader=db_rtn_data, assets=inputs['assets_idx'],
+                                     startdate=startdate.strftime("%Y%m%d"), enddate=enddate.strftime("%Y%m%d"),
+                                     rtn_dilate=inputs['rtn_dilate'])
+        res_list.append(res)
+    return res_list
 
 
 
