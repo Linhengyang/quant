1fc4709444db610e45141e2b25ff73f14c60c46a
lhy-Macbook01 <linhengyang1@outlook.com>
2024-01-09 09:49:29 +0800

    trade date extractor add

--- Code/DataLoader/remoteDB.py
+++ Code/DataLoader/remoteDB.py
@@ -38,3 +38,30 @@ def db_rtn_data(assets:list, startdate:str, enddate:str, rtn_dilate):
     assets_inds = data.index.to_list()
     
     return rtn_data, assets_inds
+
+
+
+def db_date_data(assets:list, startdate:str, enddate:str):
+    # get trade dates for assets from startdate to enddate
+    db = DatabaseConnection(localdb)
+    assert int(startdate) <= int(enddate), 'start date must no later than end date'
+
+    sql_query = '''
+    SELECT DISTINCT TRADE_DT
+    FROM aidx_eod_prices
+    WHERE TRADE_DT >= {startdate} AND TRADE_DT <= {enddate}
+    AND S_IRDCODE in {assets_tuple}
+    '''.format(startdate=int(startdate), enddate=int(enddate), assets_tuple=tuple(assets))
+    
+    raw_data = db.GetSQL(sql_query, tbl_type='pddf')['TRADE_DT'].astype(str)
+    # data = raw_data.pivot_table(index='S_IRDCODE', columns=['TRADE_DT'], values=['PCHG'])
+    # rtn_data = data.to_numpy(dtype=np.float32)
+    # assets_inds = data.index.to_list()
+    
+    return raw_data
+
+
+
+
+if __name__ == "__main__":
+    print( db_date_data(['000001.SH', '000016.SH', '000002.SH', '000009.SH', '000010.SH'], '20230101', '20230103') )
