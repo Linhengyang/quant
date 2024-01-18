5ee2d6b46155434a02954ef8c82c96f22b9e9f6d
lhy-Macbook01 <linhengyang1@outlook.com>
2024-01-18 17:38:03 +0800

    meanvar & risk manage add

--- Code/projs/asset_allocate/server_dev.py
+++ Code/projs/asset_allocate/server_dev.py
@@ -1,24 +1,16 @@
-from flask import Flask, request
-from werkzeug.middleware.proxy_fix import ProxyFix
-import json
-import re
+from flask import Flask
 import warnings
-from datetime import datetime, timedelta
-from markupsafe import escape
-from pprint import pprint
-import traceback
 import sys
 sys.dont_write_bytecode = True
 
-from Code.projs.asset_allocate.functions import *
-from Code.projs.asset_allocate.dataload import db_rtn_data, db_date_data
-from Code.Utils.Sequence import strided_slicing_w_residual
-from Code.BackTester.BT_AssetAllocate import rtn_multi_periods
+
 
 warnings.filterwarnings('ignore')
 app_name = __name__
 static_folder = "Static"
 template_folder = 'Template'
+
+
 
 asset_allocate_app = Flask(app_name, static_folder=static_folder, template_folder=template_folder)
 
@@ -26,4 +18,10 @@ asset_allocate_app = Flask(app_name, static_folder=static_folder, template_folde
 
 
 from Code.projs.asset_allocate.mean_var import mvopt_api
+from Code.projs.asset_allocate.risk_manage import risk_manage_api
+
+
+
+
 asset_allocate_app.register_blueprint(mvopt_api)
+asset_allocate_app.register_blueprint(risk_manage_api)
