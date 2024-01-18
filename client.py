875604e28a54c59b4fa8bcf51d11a2aa52304391
lhy-Macbook01 <linhengyang1@outlook.com>
2024-01-18 17:36:42 +0800

    risk manage testd

--- client.py
+++ client.py
@@ -20,12 +20,12 @@ inputs = {
         #   "risk_avers_factor":0.3,
         #   "equi_wght_vec":[0.1, 0.3, 0.2, 0.15, 0.25],
         #   "tau":0.05,
-          "low_constraints":[-10, -10, -10, -10, -10], "high_constraints":[20, 20, 20, 20, 20],
+          # "low_constraints":[-10, -10, -10, -10, -10], "high_constraints":[20, 20, 20, 20, 20],
         #   "category_mat":[[1,1,0,0,0],
         #                   [0,0,1,1,0],
         #                   [0,0,0,0,1]],
-        #   "tgt_contrib_ratio":[0.5, 0.3, 0.2],
-          "assets_idx":["000001.SH", "000016.SH", "000002.SH", "000009.SH", "000010.SH"],
+          # "tgt_contrib_ratio":[0.5, 0.3, 0.2],
+          # "assets_idx":["000001.SH", "000016.SH", "000002.SH", "000009.SH", "000010.SH"],
           # "startdate":"20230101",
           # "enddate":"20230401",
           "rtn_dilate":100,
@@ -38,33 +38,39 @@ inputs = {
                 "id":"000001.SH",
                 "lower_bound":-10,
                 "upper_bound":20,
-                "category":"index"
+                "category":"index",
+                "asset_risk_ratio":0.4
               },
               {
                 "id":"000016.SH",
                 "lower_bound":-10,
                 "upper_bound":20,
-                "category":"index"
+                "category":"index",
+                "asset_risk_ratio":0.1
               },
               {
                 "id":"000002.SH",
                 "lower_bound":-10,
                 "upper_bound":20,
-                "category":"index"
+                "category":"index",
+                "asset_risk_ratio":0.2
               },
               {
                 "id":"000009.SH",
                 "lower_bound":-10,
                 "upper_bound":20,
-                "category":"index"
+                "category":"index",
+                "asset_risk_ratio":0.1
               },
               {
                 "id":"000010.SH",
                 "lower_bound":-10,
                 "upper_bound":20,
-                "category":"index"
+                "category":"index",
+                "asset_risk_ratio":0.2
               },
           ]
           }
-r = requests.post("http://127.0.0.1:8000/asset_allocate/BT_mvopt_var_from_r", json=inputs) # post data
+# r = requests.post("http://127.0.0.1:8000/asset_allocate/BT_mvopt_var_from_r", json=inputs) # post data
+r = requests.post("http://127.0.0.1:8000/asset_allocate/risk_manage", json=inputs) # post data
 print(r.text)
