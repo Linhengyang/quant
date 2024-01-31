319e690626c75afab709d02923e06cdc3880a212
lhy-Macbook01 <linhengyang1@outlook.com>
2024-01-31 20:21:36 +0800

    seperate runner

--- Code/projs/asset_allocate/inputParser.py
+++ Code/projs/asset_allocate/inputParser.py
@@ -3,16 +3,9 @@ from operator import itemgetter
 import functools
 import typing as t
 import numpy as np
-from flask import request, Blueprint, Flask
 import warnings
-from markupsafe import escape
-from pprint import pprint
-import traceback
 import sys
 sys.dont_write_bytecode = True
-
-from Code.projs.asset_allocate.benchmark import benchmarkStrat
-from Code.projs.asset_allocate.meanvarOpt import meanarOptStrat
 
 
 
@@ -21,53 +14,37 @@ app_name = __name__
 static_folder = "Static"
 template_folder = 'Template'
 
-mvopt_api = Blueprint('mean_var', __name__)
-
-
-__all__ = [
-    "begindate",
-    "termidate",
-    "dilate",
-    "back_window_size",
-    "gapday",
-    "benchmark",
-]
-
 
 
 
 class ConstraintsCheck:
 
     def __init__(self, func: Callable) -> None:
-        self.__func = func
+        self.func = func
 
-    def __call__(self) -> Callable:
+
+    def __call__(self, *args, **kwargs) -> Callable:
         
-        @functools.wraps(self.__func)
-        def wrapper(*args, **kwargs):
-            low_bounds, upper_bounds = self.__func(*args, **kwargs)
+        low_bounds, upper_bounds = self.func(*args, **kwargs)
 
-            if isinstance(low_bounds, np.array) and isinstance(upper_bounds, np.array):
+        if isinstance(low_bounds, np.ndarray) and isinstance(upper_bounds, np.ndarray):
 
-                assert np.sum(low_bounds) <= 1.0, \
-                    "sum of low bounds of weights must be <= 1"
-                
-                assert all(low_bounds <= upper_bounds), \
-                    "all low bounds must be be smaller or equal to high bounds"
+            assert np.sum(low_bounds) <= 1.0, \
+                "sum of low bounds of weights must be <= 1"
+            
+            assert all(low_bounds <= upper_bounds), \
+                "all low bounds must be be smaller or equal to high bounds"
 
-                # assert all(low_bounds >= 0.0), \
-                #     "no short trading! low bounds must be >= 0"
-                # assert all(low_bounds <= 1.0), \
-                #     "no leverage trading! low bounds must be <= 1"
-                # assert all(upper_bounds >= 0.0), \
-                #     "no short trading! upper bounds must be >= 0"
-                # assert all(upper_bounds <= 1.0), \
-                #     "no leverage trading! upper bounds must <= 1"
-        
-            return (low_bounds, upper_bounds)
-
-        return wrapper
+            # assert all(low_bounds >= 0.0), \
+            #     "no short trading! low bounds must be >= 0"
+            # assert all(low_bounds <= 1.0), \
+            #     "no leverage trading! low bounds must be <= 1"
+            # assert all(upper_bounds >= 0.0), \
+            #     "no short trading! upper bounds must be >= 0"
+            # assert all(upper_bounds <= 1.0), \
+            #     "no leverage trading! upper bounds must <= 1"
     
+        return (low_bounds, upper_bounds)
 
 
 
@@ -78,9 +55,8 @@ def get_constraints(
         def_l_b: float = -1000.0,
         def_u_b: float = 1000.0
         ) -> t.Tuple[
-            t.Union[np.array, None],
-            t.Union[np.array, None]
-            ]:
+            t.Union[np.ndarray, None],
+            t.Union[np.ndarray, None]]:
     '''
     input:
         1. assets_dict,  {'id': {'categ':, 'l_b', 'u_b'} }
@@ -90,7 +66,7 @@ def get_constraints(
     return:
         low_bounds
         upper_bounds
-    np.array of low/upper bounds with the order of assets_idlst
+    np.array of low/upper bounds with the order of assets_idlst or None
     '''
     low_bounds, upper_bounds = [], []
 
@@ -151,7 +127,7 @@ def parseAssets2dicts(
         asset_id, categ = asset.get('id'), asset.get('category')
         l_b, u_b = asset.get('lower_bound'), asset.get('upper_bound')
 
-        assets_dict[asset_id] = [categ, l_b, u_b]
+        assets_dict[asset_id] = {'categ':categ, 'l_b':l_b, 'u_b':u_b}
 
         tbl = get_tbl_asset(asset_id, categ)
 
@@ -166,84 +142,14 @@ def parseAssets2dicts(
 
 
 
-
-def release2global(inputs: Any) -> None:
-
-    # 持仓起始日，持仓终结日，膨胀系数, 调仓周期, 基准代号, 回看窗口天数
-    global begindate, termidate, dilate, gapday, benchmark, back_window_size
-
-    begindate, termidate, dilate, gapday, benchmark, back_window_size = \
-        itemgetter(
-            "begindate",
-            "termidate",
-            "rtn_dilate",
-            "gapday",
-            "benchmark",
-            "back_window_size",
-        )(inputs)
-    
-    print(f'release begindate {begindate}, termidate {termidate}, dilate {dilate},\
-            gapday {gapday}, benchmark {benchmark}, back_window_size {back_window_size}\
-            to global')
-
-    # 类型转换
-    gapday, back_window_size, dilate = int(gapday), int(back_window_size), int(dilate)
-
-
-
-
-
-
-@mvopt_api.route('/asset_allocate/mean_var_opt', methods=['POST'])
-def runner():
-    '''
-    output:
-    {
-        'details': [res1, res2],
-        'weights': [portf_w1, portf_w2],
-        'assets_id': ['id1', 'id2'],
-        'backtest':
-            {'rtn', 'var', 'std', 'trade_days':, 'total_cost':, 
-            'gross_rtn':, 'annual_rtn':},
-        'benchmark':
-            {'rtn', 'var', 'std', 'trade_days':, 'total_cost':,
-            'gross_rtn':, 'annual_rtn':},
-        'excess':
-            {'rtn':, 'annual_rtn':}
-    }
-    '''
-
-    inputs = request.json
-
-    release2global(inputs)
-
-
-    mvopt_strat = meanarOptStrat(inputs)
-    BT_mvopt = mvopt_strat.backtest()
-
-    bchmk_strat = benchmarkStrat(benchmark)
-    BT_bchmak = bchmk_strat.backtest()
-
-    return {
-        'details': mvopt_strat.detail_solve_results,
-        'weights': mvopt_strat.portf_w_list,
-        'assets_id': mvopt_strat.assets_idlst,
-        'backtest': BT_mvopt,
-        'benchmark': BT_bchmak
-    }
-
-
-
-
-
-
-
-
-
-
 if __name__ == "__main__":
-
-    asset_allocate_app = Flask(app_name, static_folder=static_folder, template_folder=template_folder)
-    asset_allocate_app.register_blueprint(mvopt_api)
-    
-    asset_allocate_app.run(port=8000, debug=True)
+    assets_dict = {
+        '00001': {
+            'categ':'1', 'l_b':0, 'u_b':1
+        },
+        '00002': {
+            'categ':'1', 'l_b':0, 'u_b':1
+        },
+    }
+    assets_idlst = ['00001', '00002']
+    print( get_constraints(assets_dict, assets_idlst ) )
---
+++ Code/projs/asset_allocate/runner.py
@@ -0,0 +1,87 @@
+import warnings
+from operator import itemgetter
+from flask import request, Blueprint
+from typing import Any
+
+warnings.filterwarnings('ignore')
+app_name = __name__
+static_folder = "Static"
+template_folder = 'Template'
+
+mvopt_api = Blueprint('mean_var', __name__)
+
+
+__all__ = [
+    "begindate",
+    "termidate",
+    "dilate",
+    "back_window_size",
+    "gapday",
+    "benchmark",
+]
+
+
+def release2global(inputs: Any) -> None:
+
+    # 持仓起始日，持仓终结日，膨胀系数, 调仓周期, 基准代号, 回看窗口天数
+    global begindate, termidate, dilate, gapday, benchmark, back_window_size
+
+    begindate, termidate, dilate, gapday, benchmark, back_window_size = \
+        itemgetter(
+            "begindate",
+            "termidate",
+            "rtn_dilate",
+            "gapday",
+            "benchmark",
+            "back_window_size",
+        )(inputs)
+    
+    print(f'release begindate {begindate}, termidate {termidate}, dilate {dilate},\
+            gapday {gapday}, benchmark {benchmark}, back_window_size {back_window_size}\
+            to global')
+
+    # 类型转换
+    gapday, back_window_size, dilate = int(gapday), int(back_window_size), int(dilate)
+
+
+
+@mvopt_api.route('/asset_allocate/mean_var_opt', methods=['POST'])
+def runner():
+    '''
+    output:
+    {
+        'details': [res1, res2],
+        'weights': [portf_w1, portf_w2],
+        'assets_id': ['id1', 'id2'],
+        'backtest':
+            {'rtn', 'var', 'std', 'trade_days':, 'total_cost':, 
+            'gross_rtn':, 'annual_rtn':},
+        'benchmark':
+            {'rtn', 'var', 'std', 'trade_days':, 'total_cost':,
+            'gross_rtn':, 'annual_rtn':},
+        'excess':
+            {'rtn':, 'annual_rtn':}
+    }
+    '''
+
+    inputs = request.json
+
+    release2global(inputs)
+
+    from Code.projs.asset_allocate.benchmark import benchmarkStrat
+    from Code.projs.asset_allocate.meanvarOpt import meanvarOptStrat
+
+    mvopt_strat = meanvarOptStrat(inputs)
+    BT_mvopt = mvopt_strat.backtest()
+
+    bchmk_strat = benchmarkStrat(benchmark)
+    BT_bchmak = bchmk_strat.backtest()
+
+    return {
+        'details': mvopt_strat.detail_solve_results,
+        'weights': mvopt_strat.portf_w_list,
+        'assets_id': mvopt_strat.assets_idlst,
+        'backtest': BT_mvopt,
+        'benchmark': BT_bchmak
+    }
+
